500d01fdc04e0d1420655b126d38649b5ba7f772
Daniel Kambich <dkambich@purdue.edu>
2019-04-11 23:46:24 -0400

    Clean and comment Landing.js

--- src/screens/Landing.js
+++ src/screens/Landing.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 // Firebase Imports
 import fire from "../constants/config";
 // React Router Imports
@@ -9,28 +9,33 @@ class Landing extends Component {
   constructor(props) {
     super(props);
     this.redirectUser = this.redirectUser.bind(this);
+    // Register authentication state change listener
     fire.auth().onAuthStateChanged(this.redirectUser);
   }
 
   redirectUser(user) {
+    // If the user exists...
     if (user) {
+      // Get their profile data
       let profile = {
         email: user.email,
         name: user.displayName,
         photoURL: user.photoURL,
         uid: user.uid
       };
+      // Navigate to the main page, passing on user data
       this.props.history.push({
         pathname: ROUTES.MAIN,
         state: { user: profile }
       });
     } else {
+      // Otherwise, navigate to the sign in page
       this.props.history.push(ROUTES.SIGN_IN);
     }
   }
 
   render() {
-    return <div />;
+    return <Fragment />;
   }
 }
 

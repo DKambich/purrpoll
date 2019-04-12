0b6d5a3514533d4561f59107acae8f2a07281d1c
Daniel Kambich <dkambich@purdue.edu>
2019-04-12 17:42:50 -0400

    Add dialog and firebase code to delete the user's account

--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -3,7 +3,6 @@ import React, { Component, Fragment } from "react";
 import {
   AppBar,
   Avatar,
-  Button,
   Card,
   CardContent,
   Grid,
@@ -14,7 +13,13 @@ import {
   Typography,
   withStyles,
   ListItemText,
-  ListItemIcon
+  ListItemIcon,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogContentText,
+  DialogActions,
+  Button
 } from "@material-ui/core";
 import {
   ArrowBackRounded,
@@ -23,6 +28,8 @@ import {
   MoreVert
 } from "@material-ui/icons";
 // Firebase Imports
+import firebase from "firebase/app";
+import "firebase/auth";
 import fire from "../constants/config";
 // React Router Imports
 import { LANDING } from "../constants/routes";
@@ -53,26 +60,32 @@ class Profile extends Component {
     }
 
     this.state = {
-      anchorEl: null
+      menuAnchor: null,
+      dialogOpen: false
     };
 
     // Function Binding
     this.signOut = this.signOut.bind(this);
     this.deleteAccount = this.deleteAccount.bind(this);
+
     this.renderMenu = this.renderMenu.bind(this);
-    this.closeMenu = this.closeMenu.bind(this);
     this.openMenu = this.openMenu.bind(this);
+    this.closeMenu = this.closeMenu.bind(this);
+
+    this.renderDialog = this.renderDialog.bind(this);
+    this.openDialog = this.openDialog.bind(this);
+    this.closeDialog = this.closeDialog.bind(this);
   }
 
   render() {
     const { classes, history, location, theme } = this.props;
     const { user } = location.state;
-    const { anchorEl } = this.state;
-    const open = Boolean(anchorEl);
+    const { menuAnchor } = this.state;
+    const open = Boolean(menuAnchor);
 
     return (
       <Fragment>
-        <AppBar position="static" color="primary">
+        <AppBar color="primary" position="fixed">
           <Toolbar>
             <IconButton color="inherit" onClick={history.goBack}>
               <ArrowBackRounded />
@@ -84,9 +97,12 @@ class Profile extends Component {
             <IconButton color="inherit" onClick={this.openMenu}>
               <MoreVert />
             </IconButton>
-            {this.renderMenu(anchorEl, open)}
+            {this.renderMenu(menuAnchor, open)}
+            {this.renderDialog()}
           </Toolbar>
         </AppBar>
+
+        <div style={{ paddingTop: 64 }} />
 
         <Grid container direction="column" justify="center" alignItems="center">
           <Typography variant="h4">Your Voted Cats</Typography>
@@ -106,6 +122,59 @@ class Profile extends Component {
         </Grid>
       </Fragment>
     );
+  }
+
+  signOut() {
+    this.closeMenu();
+    fire.auth().signOut();
+    this.props.history.push(LANDING);
+  }
+
+  async deleteAccount() {
+    this.closeDialog();
+    let user = fire.auth().currentUser;
+    try {
+      await user.delete();
+      this.props.history.push(LANDING);
+    } catch (error) {
+      console.error(error.code, error.message);
+      let provider = new firebase.auth.GoogleAuthProvider();
+      await user.reauthenticateWithPopup(provider);
+      await user.delete();
+      this.props.history.push(LANDING);
+    }
+  }
+
+  renderDialog() {
+    return (
+      <Dialog open={this.state.dialogOpen} onClose={this.closeDialog}>
+        <DialogTitle>Delete your account?</DialogTitle>
+        <DialogContent>
+          <DialogContentText>
+            By deleting your account, all your personal data and account details
+            will be deleted and you will have to create a new account to log in.
+            Do you wish to proceed?
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={this.closeDialog} color="inherit" autoFocus>
+            Cancel
+          </Button>
+          <Button onClick={this.deleteAccount} color="secondary">
+            Confirm
+          </Button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+
+  openDialog() {
+    this.closeMenu();
+    this.setState({ dialogOpen: true });
+  }
+
+  closeDialog() {
+    this.setState({ dialogOpen: false });
   }
 
   renderMenu(element, open) {
@@ -133,7 +202,7 @@ class Profile extends Component {
           <ListItemText primary="Sign Out" />
         </MenuItem>
         {/* MenuItem to delete the user's account */}
-        <MenuItem onClick={this.deleteAccount}>
+        <MenuItem onClick={this.openDialog}>
           <ListItemIcon>
             <DeleteForever color="secondary" />
           </ListItemIcon>
@@ -147,21 +216,12 @@ class Profile extends Component {
   }
 
   openMenu(event) {
-    // this.setState({ anchorEl: event.currentTarget.children[0] });
-    this.setState({ anchorEl: event.currentTarget });
+    this.setState({ menuAnchor: event.currentTarget });
   }
 
   closeMenu() {
-    this.setState({ anchorEl: null });
+    this.setState({ menuAnchor: null });
   }
-
-  signOut() {
-    this.closeMenu();
-    fire.auth().signOut();
-    this.props.history.push(LANDING);
-  }
-
-  deleteAccount() {}
 }
 
 export default withStyles(styles, { withTheme: true })(Profile);
